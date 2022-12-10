2a4025ceec4221093a2846b6334f8dde37f5b633
Rainy <112306653+rainy-moss@users.noreply.github.com>
2022-12-10 13:38:20 -0600

    completed section 1

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
-  image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  photosTitle = 'My Favorite Pictures';
+  image1 = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqvSb60CDLfzbD6txFi5zbcOAnLP6BcCpfWgdp6td9coOA7Jz8tmXdolRdYGvfkzWViYA&usqp=CAU';
+  image2 = 'https://i.pinimg.com/736x/73/ae/99/73ae99970b6a634631f94fd18dd68aa7.jpg';
+  image3 = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcToAkH7sOraKWtckg8gXzx-3YdAf_4BV0aAli3RQwKcpcX-kTVWfAJ_mBiWs1kqKspPHZY&usqp=CAU';
 
   constructor() { }
 
